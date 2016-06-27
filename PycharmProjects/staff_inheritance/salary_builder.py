6929c8788977d7a542037c9805652ec09bc3a6ad
brianojee <brian@codeinstitute.net>
2016-06-27 16:14:46 +0100

    Adding salary_builder

---
+++ PycharmProjects/staff_inheritance/salary_builder.py
@@ -0,0 +1,9 @@
+
+from staff.developer import Developer
+
+emp1 = Developer('tom', 'bombadil', 4, 'Python')
+emp2 = Developer('Martha', 'Wainright', 4, 'Java')
+
+print emp1.details()
+
+print emp2.details()
